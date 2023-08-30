91c250776ba4e60735b399fdc19f5c8ba7b3fe8b
Rahmatjon <128359200+RahmatjonIbrohimov@users.noreply.github.com>
2023-08-30 22:21:34 +0500

    Add TestCase

--- qrCode/tests.py
+++ qrCode/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class TestCase(SimpleTestCase):
+    def test_first(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
